67718995df4cbe5a2d5152d615c551f7e9286e44
objetive218 <davidsantiagotc@gmail.com>
2025-01-06 14:34:03 -0500

    add new botton and a conditional render

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
+import { useState } from 'react'
 import './App.css'
 import Messages from './components/Messages'
 import NewMesagge from './components/NewMesagge'
 
 function App() {
-
+  const [showFormNew, setShowFormNew] = useState(false)
   return (
     <>
       <div>
+        <button onClick={() => setShowFormNew(true)}>+</button>
+        {showFormNew ? <NewMesagge setShowFormNew={setShowFormNew} /> : ""}
         <h1>Messages</h1>
-        <NewMesagge />
         <Messages />
       </div>
     </>
