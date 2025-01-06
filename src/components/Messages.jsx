7f4000dc82447b5ed227f83cecaa86fbcacbd808
objetive218 <davidsantiagotc@gmail.com>
2025-01-06 14:32:14 -0500

    remove logic to new component

--- src/components/Messages.jsx
+++ src/components/Messages.jsx
@@ -1,11 +1,10 @@
 import axios from "axios";
-import { useContext, useEffect, useState } from "react";
+import { useContext, useEffect } from "react";
 import AllMessagesContext from "../../context/AllMessagesContext";
-import ChangeForm from "./ChangeForm";
+import Message from "./message";
 
 const Messages = () => {
     const {content, setContent} = useContext(AllMessagesContext);
-    const [showForm, setShowForm] = useState(false);
     useEffect(() => {
         axios.get(`${import.meta.env.VITE_URL_DB}`).then((response) => setContent(response.data.messages)).catch(() => {
         })
@@ -15,14 +14,7 @@ const Messages = () => {
     return (
         <div>
             {content.map((e) => {
-                return (<>
-                    <h3 key={e._id}>{e.user}</h3>
-                    <h1>{e.title}</h1>
-                    <p>{e.text}</p>
-                    {/*need only active the form of one message */}
-                    <button onClick={() => setShowForm(!showForm)}>Edit</button>
-                </>
-                )
+                return <Message key={e._id} element={e}/>
             })}
         </div>
     );
